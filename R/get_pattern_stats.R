c3c412da96a7f04ec3f7377be95a8b7778cdbb11
TomSOWI <tomklotz@gmx.net>
2023-08-01 18:50:56 +0200

    add pattern stats function

--- R/get_pattern_stats.R
+++ R/get_pattern_stats.R
@@ -4,13 +4,21 @@
 #'
 #' @param text A corpus or tokens object suitable for quantedas kwic function
 #' @param pattern A dictionary or a character vector
+#' @param regex Do the patterns contain regex? TRUE | FALSE
 #'
 #' @return A dataframe with the columns pattern and match_count
 #' @export
 #'
 #' @examples
-get_pattern_stats <- function(text, pattern){
-  kwic <- kwic(toksBT17_20_clean, gruendl_patterns_to_compound(), "regex", window = 1)
+get_pattern_stats <- function(text, pattern, regex = TRUE){
+
+  if (regex == TRUE){
+    kwic <- kwic(toksBT17_20_clean, gruendl_patterns_to_compound(), "regex", window = 1)
+  }
+  else{
+    kwic <- kwic(toksBT17_20_clean, gruendl_patterns_to_compound(), window = 1)
+  }
+
   kwic$match_count <- 1
   df <- as.data.frame(kwic)
 
